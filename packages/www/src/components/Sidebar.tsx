ba87f0ebd661cedc50f4a4c7085e62c807dd1ca0
Kick Buttowski <icantsharemyemail>
2025-08-12 08:23:16 +0530

    feat: fix sluggish loader in sidebar

--- packages/www/src/components/Sidebar.tsx
+++ packages/www/src/components/Sidebar.tsx
@@ -3,17 +3,10 @@ import { LogIn as LoginIcon } from "lucide-react";
 import { Link } from "@tanstack/react-router";
 import { Route } from "@/routes/__root";
 import { Conversation } from "@llmchat/core/conversation/conversation";
-import * as Tooltip from "@radix-ui/react-tooltip";
-import { LoadingIcon } from "./LoadingIcon";
-import { EllipsisVertical as OptionsIcon } from "lucide-react";
-import { AnimatePresence, motion } from "motion/react";
-import { useHover } from "@uidotdev/usehooks";
-import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
-import { useMemo, useState } from "react";
-import { Trash2 as DeleteIcon, SquarePen as EditIcon } from "lucide-react";
-import { useUIStore } from "@/stores/uiStore";
+import { useMemo } from "react";
 import { SidebarToggle } from "@/components/SidebarToggle";
 import { useConversationStore } from "@/stores/conversationStore";
+import { ConversationLink } from "@/components/ConversationLink";
 
 type Props = {
   conversation: Conversation.Entity[];
@@ -116,107 +109,5 @@ export function Sidebar(props: Props) {
         )}
       </div>
     </div>
-  );
-}
-
-type ConversationProps = {
-  conversation: Conversation.Entity;
-  generating: boolean;
-};
-
-function ConversationLink(props: ConversationProps) {
-  const { conversation, generating } = props;
-  const [ref, hovering] = useHover();
-  const [open, setOpen] = useState(false);
-
-  return (
-    <Link
-      ref={ref}
-      className="hover:bg-bg px-4 py-2 rounded-md flex gap-4 items-center"
-      to="/conversation/$conversationID"
-      params={{ conversationID: conversation.id }}
-      activeProps={{ className: "bg-bg" }}
-    >
-      <Tooltip.Provider>
-        <Tooltip.Root>
-          <Tooltip.Trigger asChild>
-            <span className="truncate">{conversation.title}</span>
-          </Tooltip.Trigger>
-          <Tooltip.Portal>
-            <Tooltip.Content
-              className="bg-bg px-2 py-1 rounded-md border border-border max-w-[320px]"
-              side="bottom"
-            >
-              {conversation.title}
-            </Tooltip.Content>
-          </Tooltip.Portal>
-        </Tooltip.Root>
-      </Tooltip.Provider>
-      {generating && (
-        <LoadingIcon className="shrink-0 text-brand/40 fill-white ml-auto" />
-      )}
-      <AnimatePresence>
-        {(hovering || open) && !generating && (
-          <ConversationOptions setOpen={setOpen} conversation={conversation} />
-        )}
-      </AnimatePresence>
-    </Link>
-  );
-}
-
-type ConversationOptionsProps = {
-  setOpen: (open: boolean) => void;
-  conversation: Conversation.Entity;
-};
-
-function ConversationOptions(props: ConversationOptionsProps) {
-  const setDialog = useUIStore().setDialog;
-
-  return (
-    <DropdownMenu.Root onOpenChange={props.setOpen}>
-      <DropdownMenu.Trigger asChild>
-        <motion.button
-          initial={{ x: 10 }}
-          animate={{ x: 0 }}
-          transition={{
-            duration: 0.1,
-            ease: "easeInOut",
-          }}
-          className="shrink-0 ml-auto p-1 hover:bg-bg-2 rounded-md"
-        >
-          <OptionsIcon size={16} />
-        </motion.button>
-      </DropdownMenu.Trigger>
-      <DropdownMenu.Portal>
-        <DropdownMenu.Content className="bg-bg border border-border p-3 px-2 rounded-md flex flex-col gap-2">
-          <DropdownMenu.Item
-            className="flex gap-3 px-2 py-1 rounded-md items-center data-highlighted:outline-none data-highlighted:bg-bg-2 cursor-pointer"
-            onClick={(e) => {
-              e.stopPropagation();
-              setDialog({
-                type: "rename_conversation",
-                data: props.conversation,
-              });
-            }}
-          >
-            <EditIcon size={16} />
-            <span>Rename</span>
-          </DropdownMenu.Item>
-          <DropdownMenu.Item
-            className="flex gap-3 px-2 py-1 rounded-md items-center data-highlighted:outline-none data-highlighted:bg-danger/5 cursor-pointer text-danger"
-            onClick={(e) => {
-              e.stopPropagation();
-              setDialog({
-                type: "delete_conversation",
-                data: props.conversation,
-              });
-            }}
-          >
-            <DeleteIcon size={16} />
-            <span>Delete</span>
-          </DropdownMenu.Item>
-        </DropdownMenu.Content>
-      </DropdownMenu.Portal>
-    </DropdownMenu.Root>
   );
 }
