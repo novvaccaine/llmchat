c9f6b5df6d697dbc7d6674ad87ee91983ddd37f0
Kick Buttowski <icantsharemyemail>
2025-08-23 10:05:14 +0530

    feat: only show progress bar for slow routes

--- packages/www/src/client.tsx
+++ packages/www/src/client.tsx
@@ -10,12 +10,19 @@ import { useUIStore } from "./stores/uiStore";
 NProgress.configure({ showSpinner: false });
 
 const router = createRouter();
+let nprogressTimer: ReturnType<typeof setTimeout>;
 
+// only show the NProgress bar for route changes taking longer than 2 seconds
 router.subscribe("onBeforeLoad", ({ fromLocation, pathChanged }) => {
   useUIStore.getState().closeDrawer();
-  fromLocation && pathChanged && NProgress.start();
+  if (fromLocation && pathChanged) {
+    nprogressTimer = setTimeout(() => {
+      NProgress.start();
+    }, 2000);
+  }
 });
 router.subscribe("onLoad", () => {
+  clearTimeout(nprogressTimer);
   NProgress.done();
 });
 
